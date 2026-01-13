781f9c2b47d1e26da504281e256b7090cfbdf4bf
Jesus Muller <jhzapata.dev@gmail.com>
2026-01-13 17:15:01 -0300

    adding header to navigate

---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import './header.css';
+
+const Navbar = () => {
+    return (
+        <header className="header">
+            <Link href='/' className='link'>
+                <span className='logo'>JM</span>
+            </Link>
+            <div>Jesús Müller</div>
+        </header>
+    )
+};
+
+export default Navbar;
