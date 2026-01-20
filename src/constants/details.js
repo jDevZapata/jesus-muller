b8c836f5d586a1bc8916ca66b8e9a863192a96b6
Jesus Muller <jhzapata.dev@gmail.com>
2026-01-20 17:10:23 -0300

    adding details page

---
+++ src/constants/details.js
@@ -0,0 +1,253 @@
+const details = [
+  {
+    id: 2013,
+    name: "Thomson Reuters",
+    img: "thomson",
+    description:
+      "Thomson Reuters / La Ley Argentina es una empresa dedicada principalmente a la venta de libros para abogados y productos digitales que facilitan la búsqueda dentro de una extensa base de datos de jurisprudencia, así como el enriquecimiento de los textos encontrados.",
+    myExperience: [
+      "Thomson fue la primera empresa que me brindó la oportunidad de adentrarme en el mundo del desarrollo y donde tuve el placer de conocer a grandes personas. Aquí, aprendí rápidamente la importancia del trabajo en equipo y cómo adaptarse a las presiones diarias de la industria del desarrollo.",
+      "En Thomson, gracias a mi buen desempeño fui ascendido a desarrollador Semi Senior en mis primeros 6 meses de trabajo. Siempre me consideraron para enfrentar los desafíos más grandes dentro de la empresa.",
+      "Tuve mi primera experiencia liderando un proyecto pequeño, Westlaw India, donde adquirí una valiosa experiencia al interactuar directamente con el cliente y al contribuir en la elaboración y refinamiento de los requisitos.",
+      "Aprendí mucho sobre habilidades blandas, metodologías ágiles de trabajo como Scrum o Kanban, el manejo de Jira y la estimación en story points, entre otros aspectos.",
+      "Debido a que las aplicaciones web que desarrollé requieren de accesos, lo que puedo hacer es linkear guías donde se puede tener una visual de las apps desarrolladas.",
+    ],
+    experienceLinks: [
+      {
+        id: 1001,
+        label: "La Ley Online",
+        link: "https://support.rg.thomsonreuters.com/laley/Gu%C3%ADadeuso.pdf",
+      },
+      {
+        id: 1002,
+        label: "Westlaw Uk",
+        link: "https://www.nup.ac.cy/wp-content/uploads/2020/09/Westlaw-UK-Student-User-Guide.pdf",
+      },
+      {
+        id: 1003,
+        label: "Westlaw India",
+        link: "https://main.sci.gov.in/pdf/Library/Westlaw%20India%20Manual.pdf",
+      },
+    ],
+    technologies: [
+      "HTML4",
+      "XHTML",
+      "CSS",
+      "Javascript",
+      "Jquery",
+      "Apache velocicty",
+      "XML",
+      "XSLT",
+      "Novus",
+      "Ajax",
+      "TFS",
+      "Scrum",
+      "Kanban",
+      "Jira",
+      "Jenkins"
+    ],
+    projects: [
+      "La Ley (Argentina, Brasil, Uruguay, Chile, Perú)",
+      "Westlaw UK",
+      "Westlaw India",
+    ],
+    workedTime: { from: "2013", to: "2016" },
+  },
+  {
+    id: 2016,
+    name: "Softtek",
+    img: "softtek",
+    description:
+      "Softtek es una empresa líder en servicios de tecnología y soluciones de negocio, comprometida con la transformación digital de las organizaciones. Desde su fundación en 1982 en México, ha sido pionera en ofrecer servicios de vanguardia que impulsan la eficiencia operativa y la innovación en diversas industrias.",
+    myExperience: [
+      "En Softtek, tuve la oportunidad de trabajar con tecnologías de vanguardia y colaborar con clientes de diversos sectores gracias a la amplia cartera de servicios de la empresa en el área de tecnología de la información.",
+      "Durante casi dos años, fui consultor para OSDE, una empresa de servicios de salud, donde formé parte de equipos para proyectos de modernización del sitio web y la implementación de Google Analytics en toda la plataforma.",
+      "También participé en el desarrollo de una aplicación web interna para visualizar estadísticas de las interacciones de los usuarios con las secciones de la web, proporcionando análisis en forma de gráficos que facilitaron la toma de decisiones por parte del equipo directivo. En este proyecto, contribuí al diseño de servicios web SOAP consumidos por la interfaz de usuario que estábamos construyendo simultáneamente.",
+    ],
+    experienceLinks: [
+      {
+        id: 1004,
+        label: "OSDE",
+        link: "https://www.osde.com.ar/index.html",
+      }
+    ],
+    technologies: [
+      "HTML5",
+      "CSS3",
+      "Angular",
+      "Javascript",
+      "Typescript",
+      "ES5",
+      "Jquery",
+      "XML",
+      "Json",
+      "Java",
+      "Hibernate",
+      "SQL",
+      "Tortoise SVN",
+      "Git",
+      "Postman",
+      "Win SCP",
+      "Scrum",
+      "Kanban",
+      "Jira",
+      "Trello"
+    ],
+    projects: [
+      "OSDE"
+    ],
+    workedTime: { from: "2016", to: "2018" },
+  },
+  {
+    id: 2018,
+    name: "Globant",
+    img: "globant",
+    description:
+      "Globant es una empresa multinacional de tecnología y servicios de consultoría con sede en Argentina. Fundada en 2003, Globant se ha destacado como líder en la industria de la transformación digital, ofreciendo servicios de vanguardia en áreas como desarrollo de software, experiencia del usuario, inteligencia artificial, realidad aumentada y más.",
+    myExperience: [
+      "En Globant, tuve la oportunidad de colaborar con equipos multiculturales y trabajar completamente en inglés. Además, adquirí conocimientos sobre las mejores prácticas en desarrollo.",
+      "Durante mi tiempo en Globant, trabajé como consultor para JP Morgan, uno de los bancos más grandes del mundo, en proyectos internos sujetos a confidencialidad. Aunque no puedo compartir detalles específicos debido a acuerdos de confidencialidad, puedo mencionar que implementamos las mejores prácticas en desarrollo, como TDD, Redux, Pair programming, Peer review, CI/CD y despliegues automáticos en la nube.",
+      "Mi experiencia en Globant también me permitió conocer personas maravillosas y sumergirme en culturas enriquecedoras, trabajando codo a codo con personas de la India, Francia y Suiza.",
+    ],
+    experienceLinks: [
+      {
+        id: 1005,
+        label: "JP Morgan",
+        link: "https://www.jpmorgan.com/AR/en/about-us",
+      }
+    ],
+    technologies: [
+      "HTML5",
+      "CSS3",
+      "Styled Components",
+      "React",
+      "Javascript",
+      "ES6",
+      "Typescript",
+      "JP Morgan CL",
+      "redux",
+      "jest",
+      "enzyme",
+      "webpack",
+      "TDD",
+      "Testing coverage",
+      "babel",
+      "fetch",
+      "Json",
+      "Git",
+      "Bitbucket",
+      "Postman",
+      "Win SCP",
+      "Scrum",
+      "Kanban",
+      "Jira",
+      "Jenkins"
+    ],
+    workedTime: { from: "2018", to: "2019" },
+  },
+  {
+    id: 2019,
+    name: "Baufest",
+    img: "baufest",
+    description:
+      "Baufest es una empresa de servicios de tecnología con una amplia trayectoria en el mercado. Fundada en Argentina en 1991, Baufest se ha expandido a nivel global, estableciendo operaciones en varios países de América Latina, Estados Unidos y Europa. La empresa se especializa en ofrecer soluciones tecnológicas innovadoras y personalizadas para satisfacer las necesidades de sus clientes en áreas como desarrollo de software, consultoría tecnológica, inteligencia artificial, transformación digital y más.",
+    myExperience: [
+      "En Baufest, tuve el privilegio de colaborar con diversos clientes, incluyendo a Warner, ICBC, Pont y en una aplicación interna llamada Bit Certs.",
+      "Durante mi primer año, trabajé en proyectos para Warner. desarrollando aplicaciones web internas para la gestión de contratos publicitarios dentro de programas de la cadena, como ESPN, Cartoon Network, Nickelodeon, entre otros (utilizando Angular).",
+      "En los dos años siguientes, formé parte del equipo que trabajó para ICBC (Banco Industrial y Comercial de China), donde contribuímos al desarrollo de varias aplicaciones, incluyendo el Legajo Digital para los socios del banco y una aplicación móvil híbrida llamada Préstamos Prendarios para concesionarios asociados al banco (utilizando Angular).",
+      "Desde el año 2022, participé brevemente en el proyecto Nera (Préstamos Agro) del Banco Galicia, para luego unirme al proyecto PONT (Préstamos Agro de un grupo privado) y, en los últimos meses, trabajar en Bit Certs (certificados digitales verificables en la blockchain de Ethereum) (utilizando React).",
+      "En todos estos proyectos, nos dedicamos a mantener rigurosamente las mejores prácticas de desarrollo. Además, tuve el honor de desempeñar roles de tutor y profesor en programas de entrenamiento intensivo, realizar presentaciones de demostración a clientes, brindar apoyo técnico a mis compañeros a través de clases grupales, y también liderar y ser referente técnico frontend en los equipos en los que trabajé y también de la empresa."
+    ],
+    experienceLinks: [
+      {
+        id: 1006,
+        label: "ICBC",
+        link: "https://www.icbc.com.ar/personas"
+      },{
+        id: 1007,
+        label: "Warner",
+        link: "https://www.international.tbs.com/"
+      },{
+        id: 1008,
+        label: "BIT Certs",
+        link: "https://www.linkedin.com/posts/baufest_qu%C3%A9-es-el-bit-y-las-bitcerts-activity-7133841925576568832-rlq3/?trk=public_profile_like_view&originalSubdomain=es",
+      }
+    ],
+    technologies: [
+      "HTML5",
+      "CSS3",
+      "Angular",
+      "React",
+      "Javascript",
+      "Typescript",
+      "ES6",
+      "MUI",
+      "Bootstrap",
+      "Material design",
+      "ICBC CL",
+      "Redux",
+      "Formik",
+      "Jest",
+      "React testing library",
+      "Karma",
+      "Jazmine",
+      "CRA",
+      "Rxjs",
+      "i18n",
+      "axios",
+      "Git",
+      "Postman",
+      "Scrum",
+      "Kanban",
+      "Jira",
+      "Miro"
+    ],
+    projects: [
+      "Warner", "ICBC Legajo Digital", "ICBC Prendarios", "Nera", "Pont", "Bit certs"
+    ],
+    workedTime: { from: "2019", to: "2024" },
+  },{
+    id: 2024,
+    name: "Softtek",
+    img: "softtek",
+    description:
+      "Softtek es una empresa líder en servicios de tecnología y soluciones de negocio, comprometida con la transformación digital de las organizaciones. Desde su fundación en 1982 en México, ha sido pionera en ofrecer servicios de vanguardia que impulsan la eficiencia operativa y la innovación en diversas industrias.",
+    myExperience: [
+      "En esta nueva etapa en Softtek, tuve la oportunidad de trabajar con tecnologías modernas y en la nube de azure para Techint y uno de sus productos estrella Premo V2.",
+      "Durante casi dos años, fui consultor para Techint, una empresa de importación de materiales industriales, donde formé parte del equipo de PREMO para mejorar la experiencia de usuario del sitio web, la mejora de la performance de mismo y la implementación de nuevas funcionalidades y requerimientos solicitados por el cliente.",
+      "También participé en el desarrollo de un plugin integrado con el ecosistema de la aplicación web para agilizar la generaración de las invitaciones a cotizar para los proveedores y actualizar el estado de las mismas sin necesidad de salir del outlook actulizando el estado de las cotizaciones en tiempo real.",
+    ],
+    experienceLinks: [
+      {
+        id: 1005,
+        label: "Techint",
+        link: "https://www.techint.com/es",
+      }
+    ],
+    technologies: [
+      "HTML5",
+      "CSS3",
+      "React",
+      "Javascript",
+      "Typescript",
+      "Axios",
+      "MUI",
+      "Azure",
+      "yo-office",
+      "Recoil",
+      "ES5",
+      "Json",
+      "Git",
+      "Postman",
+      "Scrum",
+      "Kanban",
+      "Jira",
+    ],
+    projects: [
+      "OSDE"
+    ],
+    workedTime: { from: "2024"},
+  }
+];
+
+export default details;
